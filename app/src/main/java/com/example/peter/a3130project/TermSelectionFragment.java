aea8e07c835e979df0f5503c3097c919d242f4d4
Joanna <18332426+jbstks@users.noreply.github.com>
2018-06-13 15:55:04 -0300

    Adding term selection
    Doesn't quite work

--- app/src/main/java/com/example/peter/a3130project/TermSelectionFragment.java
+++ app/src/main/java/com/example/peter/a3130project/TermSelectionFragment.java
@@ -1,5 +1,6 @@
 package com.example.peter.a3130project;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,5 +19,13 @@ public class TermSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_term, container, false);
+    }
+
+    /** Called when the user taps a course card */
+    public void viewTermInfo(View view) {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.putExtra("semester", "Winter");
+        intent.putExtra("year", "2018");
+        startActivity(intent);
     }
 }
