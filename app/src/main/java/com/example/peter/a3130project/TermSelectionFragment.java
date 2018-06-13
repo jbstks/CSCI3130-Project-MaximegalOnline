275621227d49696da80dfc0b2614cfdd0f35cba1
Joanna <18332426+jbstks@users.noreply.github.com>
2018-06-13 15:08:31 -0300

    Initial Commit of Term Activity
    Deleted activity_main2.xml as it was no longer needed.

---
+++ app/src/main/java/com/example/peter/a3130project/TermSelectionFragment.java
@@ -0,0 +1,22 @@
+package com.example.peter.a3130project;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TermSelectionFragment extends Fragment {
+
+    public TermSelectionFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_term, container, false);
+    }
+}
