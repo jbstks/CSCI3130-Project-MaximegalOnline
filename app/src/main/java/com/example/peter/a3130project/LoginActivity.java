7ec0d683de4aa197a42b0391f5f8b740ad26b2de
PeterQLee <leep1995@gmail.com>
2018-06-13 14:54:35 -0300

    PL&AC: [fix] closed keyboard on button press for login #36

--- app/src/main/java/com/example/peter/a3130project/LoginActivity.java
+++ app/src/main/java/com/example/peter/a3130project/LoginActivity.java
@@ -2,6 +2,7 @@
 package com.example.peter.a3130project;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
@@ -18,6 +19,7 @@ import android.view.View;
 
 import android.view.inputmethod.EditorInfo;
 
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -75,6 +77,11 @@ public class LoginActivity extends AppCompatActivity implements android.support.
 	   ---------
 	   Initiates login.
 	 */
+        View curview = this.getCurrentFocus();
+        if (curview != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
         attemptLogin();
     }
 
