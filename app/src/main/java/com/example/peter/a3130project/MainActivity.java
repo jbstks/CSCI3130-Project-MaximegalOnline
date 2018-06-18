36954da10a7d0acf2f46fc185ec7125cca580807
Dawson Wilson <dw666766@dal.ca>
2018-06-18 15:44:32 -0300

    putting the initial stuff needed for firebase integration.

--- app/src/main/java/com/example/peter/a3130project/MainActivity.java
+++ app/src/main/java/com/example/peter/a3130project/MainActivity.java
@@ -10,11 +10,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -70,6 +77,42 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        //Database setup
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("available_courses");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                // TODO make this an arrayList?
+                // String value = dataSnapshot.getValue(String.class);
+                // Log.d("MainActivity", "Value is: " + value);
+
+                /* Sample way of querying database data
+                // Find all dinosaurs whose height is exactly 25 meters.
+                    var ref = firebase.database().ref("dinosaurs");
+                    ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
+                      console.log(snapshot.key);
+                    });
+                */
+
+                // TODO query out only the courses that are the correct semester
+                // we would query
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MainActivity", "Failed to read value.", error.toException());
+            }
+        });
 
     }
 
