8ed4f8a42e42c24b16172470684b810f52122779
Doosun <dawsonw72@gmail.com>
2018-06-17 23:00:00 -0300

    created the ability to see a scrolling term selection screen
    implementation on getting into the correct screen isn't created yet. Will work on creating espresso cases as interface is now defined.

---
+++ app/src/main/java/com/example/peter/a3130project/RVAdapter.java
@@ -0,0 +1,62 @@
+package com.example.peter.a3130project;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.TermViewHolder>{
+
+    @Override
+    public TermViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_term, viewGroup, false);
+        TermViewHolder pvh = new TermViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(TermViewHolder termViewHolder, int i) {
+        // This refers to the public class Term
+        termViewHolder.term_semester.setText(terms.get(i).semester);
+        termViewHolder.term_year.setText(terms.get(i).year);
+    }
+
+    @Override
+    public int getItemCount() {
+        return terms.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
+    public static class TermViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView term_semester;
+        TextView term_year;
+
+
+        TermViewHolder(View itemView) {
+            super(itemView);
+            cv = itemView.findViewById(R.id.cv);
+            term_semester = itemView.findViewById(R.id.term_semester);
+            term_year = itemView.findViewById(R.id.term_year);
+        }
+    }
+
+    List<Term> terms;
+
+    RVAdapter(List<Term> terms) {
+        this.terms = terms;
+    }
+}
