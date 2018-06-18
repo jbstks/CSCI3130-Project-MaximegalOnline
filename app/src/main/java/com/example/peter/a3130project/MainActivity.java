fcf44441c761d5d0a23e7a7c29495a44a6e6a00a
Dawson Wilson <dw666766@dal.ca>
2018-06-18 17:14:38 -0300

    switching to listening for a single event, instead of creating a bunch of listeners.

--- app/src/main/java/com/example/peter/a3130project/MainActivity.java
+++ app/src/main/java/com/example/peter/a3130project/MainActivity.java
@@ -83,15 +83,16 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("available_courses");
 
+
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
 
                 // TODO make this an arrayList?
-                String value = dataSnapshot.getValue(String.class);
+                // Course value = dataSnapshot.getValue(Course.class);
                 // Log.d("MainActivity", "Value is: " + value);
 
                 /* Sample way of querying database data
