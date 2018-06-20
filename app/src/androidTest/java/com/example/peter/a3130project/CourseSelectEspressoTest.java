1c9f67158583d01c924c3243e36a5dad3c214824
Doosun <dawsonw72@gmail.com>
2018-06-19 21:18:02 -0300

    Finished test espresso case for the Course viewing screen.

---
+++ app/src/androidTest/java/com/example/peter/a3130project/CourseSelectEspressoTest.java
@@ -0,0 +1,92 @@
+package com.example.peter.a3130project;
+
+import android.os.SystemClock;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.CardView;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+
+public class CourseSelectEspressoTest {
+
+    @Rule
+    public ActivityTestRule<TermActivity> activityrule =
+            new ActivityTestRule<>(TermActivity.class);
+
+    @Test
+    public void TestWinter2018FirstCourse() {
+        onView(withId(R.id.rv)).perform(actionOnItemAtPosition(0, click()));
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
+                .check(matches(withText("Winter 2018")));
+        SystemClock.sleep(1500);
+        onView(withId(R.id.course_rv)).perform(actionOnItemAtPosition(0, click()));
+        SystemClock.sleep(1500);
+    }
+
+    @Test
+    public void TestWinter2018CS1() {
+        onView(withId(R.id.rv)).perform(actionOnItemAtPosition(0, click()));
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
+                .check(matches(withText("Winter 2018")));
+        SystemClock.sleep(1500);
+        onView(withId(R.id.course_rv))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText("Computer Science 1")), click()));
+        SystemClock.sleep(1500);
+    }
+
+    @Test
+    public void TestSummer2018SEid() {
+        onView(withId(R.id.rv)).perform(actionOnItemAtPosition(1, click()));
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
+                .check(matches(withText("Summer 2018")));
+        SystemClock.sleep(1500);
+        /*onView(withId(R.id.course_rv))
+                .check(matches(hasDescendant(withText("42665")))).perform(click());*/
+        onView(withId(R.id.course_rv))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText("42665")), click()));
+        onView(withId(R.id.courseInfo_id)).check(matches(withText("42665")));
+        onView(withId(R.id.courseInfo_code)).check(matches(withText("Software Engineering")));
+        onView(withId(R.id.courseInfo_name)).check(matches(withText("CSCI3130")));
+        SystemClock.sleep(1500);
+    }
+
+    @Test
+    public void TestSummer2018SEName() {
+        onView(withId(R.id.rv)).perform(actionOnItemAtPosition(1, click()));
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.toolbar))))
+                .check(matches(withText("Summer 2018")));
+        SystemClock.sleep(1500);
+        /*onView(withId(R.id.course_rv))
+                .check(matches(hasDescendant(withText("42665")))).perform(click());*/
+        onView(withId(R.id.course_rv))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText("Software Engineering")), click()));
+        onView(withId(R.id.courseInfo_id)).check(matches(withText("42665")));
+        onView(withId(R.id.courseInfo_code)).check(matches(withText("Software Engineering")));
+        onView(withId(R.id.courseInfo_name)).check(matches(withText("CSCI3130")));
+        SystemClock.sleep(1500);
+    }
+
+}
