4507128b608e488a598291f9fda7f5a349580b60
Peter Lee <peter@TEE63.WPA.Dal.Ca>
2018-06-25 15:50:56 -0300

    PL_MG: Added outline implementation for registration

---
+++ app/src/main/java/com/example/peter/a3130project/register/CourseRegistrationUI.java
@@ -0,0 +1,11 @@
+public class CourseRegistrationUI extends CourseRegistration{
+
+    public CourseRegistrationUI(FirebaseUser user) {
+	//  Fills in current_courses with the firebase instance.
+    }
+    public void do_register(Course course, FirebaseUser user) {
+	/* Adds the course to the database*/
+	
+    }
+	
+}
