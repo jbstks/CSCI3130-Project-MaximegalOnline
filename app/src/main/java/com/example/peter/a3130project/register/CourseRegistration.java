633b73d229f8ecdbfa220236b6e8c911c49973fe
Peter Lee <peter@TEE63.WPA.Dal.Ca>
2018-06-27 14:58:58 -0300

    PL_MG: [Backend] Added registration Junit Tests

--- app/src/main/java/com/example/peter/a3130project/register/CourseRegistration.java
+++ app/src/main/java/com/example/peter/a3130project/register/CourseRegistration.java
@@ -1,11 +1,25 @@
-public class CourseRegistration{
+package com.example.peter.a3130project.register;
+import com.example.peter.a3130project.Course;
+
+import java.util.ArrayList;
+
+public class CourseRegistration {
     private ArrayList<Course> current_courses;
+
+    public CourseRegistration() {
+    }
+
+
+    public CourseRegistration(ArrayList<Course> current_courses){
+        this.current_courses = current_courses;
+    }
 
     public ArrayList<Course> attempt_register(Course course) {
 	// Steps:
 	// 1. sort current_course by times
 	// 2. see whether course conflicts with any
 	// 3. Give the proper response to which are conflicting.
+	return null;
     }
     
 }
