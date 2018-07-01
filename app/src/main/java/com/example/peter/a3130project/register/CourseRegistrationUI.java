ed698ff3bda5c9ab00d5d6e3ba8c7dd9e80d343b
mgosse442 <megangosse@gmail.com>
2018-07-01 14:00:41 -0300

    Get CRNs for student's current courses from firebase

--- app/src/main/java/com/example/peter/a3130project/register/CourseRegistrationUI.java
+++ app/src/main/java/com/example/peter/a3130project/register/CourseRegistrationUI.java
@@ -1,17 +1,47 @@
 package com.example.peter.a3130project.register;
+import com.example.peter.a3130project.CourseSection;
 
 import com.example.peter.a3130project.Course;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class CourseRegistrationUI extends CourseRegistration{
 
     public CourseRegistrationUI(FirebaseUser user) {
         super();
         //  Fills in current_courses with the firebase instance.
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("students");
+
+        // TODO setup B00 numbers for each user and query for them here
+        DatabaseReference currentCoursesRef = ref.child("B00123456").child("courses").child("current");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //get CRNs
+                ArrayList<String> CRNs = new ArrayList<String>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    CRNs.add(snapshot.getValue(String.class));
+                }
+
+                // TODO get courseSection info and set current_courses
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
     }
     public void do_register(Course course, FirebaseUser user) {
 	/* Adds the course to the database*/
 	
     }
-	
 }
