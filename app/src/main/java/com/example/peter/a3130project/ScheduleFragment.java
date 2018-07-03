d10c318d0bee1e4084314ae38f8fc98b15437073
Bradley Garagan <constxd@gmail.com>
2018-07-03 13:03:25 -0300

    populate the schedule view with course data

--- app/src/main/java/com/example/peter/a3130project/ScheduleFragment.java
+++ app/src/main/java/com/example/peter/a3130project/ScheduleFragment.java
@@ -1,15 +1,27 @@
 package com.example.peter.a3130project;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ScheduleFragment extends Fragment {
+
+    private View view;
 
     public ScheduleFragment() {
     }
@@ -18,9 +30,89 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.schedule, container, false);
+        view = inflater.inflate(R.layout.schedule, container, false);
 
         return view;
+    }
+
+    public void update(List<Course> courses) {
+
+        if (view == null)
+            return;
+
+        final LinearLayout[] dayViews = {
+                view.findViewById(R.id.monday),
+                view.findViewById(R.id.tuesday),
+                view.findViewById(R.id.wednesday),
+                view.findViewById(R.id.thursday),
+                view.findViewById(R.id.friday)
+        };
+
+        final ArrayList<String> days = new ArrayList<String>(Arrays.asList("mon", "tue", "wed", "thu", "fri"));
+
+        final String[] colors = { "#ffe8e8", "#f0f0da", "#d3eabb", "#a9c4a0" };
+        int colorIndex = 0;
+
+        final ArrayList<ArrayList<ScheduleEntry>> schedule = new ArrayList<>();
+        for (int i = 0; i < 5; ++i) {
+            schedule.add(new ArrayList<ScheduleEntry>());
+            dayViews[i].removeViews(1, dayViews[i].getChildCount() - 1);
+        }
+
+        for (Course c : courses) {
+            int color = Color.parseColor(colors[colorIndex++ % colors.length]);
+            for (CourseTime t : c.times) {
+                int i = days.indexOf(t.day);
+                int start = Integer.parseInt(t.startTime);
+                int end = Integer.parseInt(t.endTime);
+                schedule.get(i).add(new ScheduleEntry(c.code, start, end, t.location, color));
+            }
+        }
+
+        for (int i = 0; i < 5; ++i) {
+            ArrayList<ScheduleEntry> day = schedule.get(i);
+            Collections.sort(day);
+
+            int time = 805;
+
+            for (ScheduleEntry e : day) {
+                TextView tv = new TextView(getActivity().getApplicationContext());
+
+                int duration = 60 * ((e.end / 100) - (e.start / 100)) + (e.end % 100 - e.start % 100);
+                int before =  60 * ((e.start / 100) - (time / 100)) + (e.start % 100 - time % 100);
+
+                duration = ((duration + 25) / 30) * 30;
+                before = ((before + 25) / 30) * 30;
+
+                final float scale = getResources().getDisplayMetrics().density;
+                int blockSize = (int) (80 * scale + 0.5f);
+                int height = blockSize * duration / 60;
+                int margin = blockSize * before / 60;
+
+                Log.d("tv", "blockSize = " + blockSize);
+                Log.d("tv", "start = " + e.start);
+                Log.d("tv", "end = " + e.end);
+                Log.d("tv", "height = " + height);
+                Log.d("tv", "margin = " + margin);
+                Log.d("tv", "duration = " + duration);
+
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, height);
+                LinearLayout.LayoutParams mlp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, margin);
+
+                View v = new View(getActivity().getApplicationContext());
+                v.setLayoutParams(mlp);
+                dayViews[i].addView(v);
+
+
+                tv.setBackgroundColor(e.color);
+                tv.setTextColor(Color.parseColor("#000000"));
+                tv.setLayoutParams(lp);
+                tv.setText(e.name + "\n" + e.location);
+                dayViews[i].addView(tv);
+
+                time = e.end;
+            }
+        }
     }
 
 }
