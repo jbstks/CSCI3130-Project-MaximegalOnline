21fa5374e935004f974ae0755418dd7df817983d
Dawson <dawsonw72@gmail.com>
2018-07-03 20:05:34 -0300

    Fixing espresso test to work with our solution. Then change the UI order of the sections card view.

--- app/src/androidTest/java/com/example/peter/a3130project/SectionEspressoTest.java
+++ app/src/androidTest/java/com/example/peter/a3130project/SectionEspressoTest.java
@@ -36,15 +36,34 @@ public class SectionEspressoTest {
     @Test
     public void TestSectionList() {
 
-        onView(withId(R.id.rv))
-                .perform(RecyclerViewActions.actionOnItem(
-                        hasDescendant(withText("Summer 2018")), click()));
+        SystemClock.sleep(1500);
+
+        //onRecyclerItemView(R.id.item_title, withText("Test"),  withId(R.id.item_content))
+        //        .matches(check(withText("Test Content")));
+
+        onView(withId(R.id.rv)).perform(actionOnItemAtPosition(1, click()));
+
+        SystemClock.sleep(1500);
 
         onView(withId(R.id.course_rv))
                 .perform(RecyclerViewActions.actionOnItem(
                         hasDescendant(withText("Algorithms")), click()));
 
-        onView(withId(R.id.sections_rv)).perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("42343"))));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("42344"))));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("Norbert Zeh"))));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("Meng He"))));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("01"))));
+        onView(withId(R.id.sections_rv))
+                .check(matches(hasDescendant(withText("02"))));
+        //onView(withId(R.id.courseInfo_id)).check(matches(withText("42665")));
+        //onView(withId(R.id.courseInfo_code)).check(matches(withText("CSCI3130")));
+        //onView(withId(R.id.courseInfo_name)).check(matches(withText("Software Engineering")));
         SystemClock.sleep(1500);
 
     }
