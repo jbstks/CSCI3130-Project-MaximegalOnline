da7f2285fec6b332a4b3824a3cfc1649d0d54f7d
PeterQLee <leep1995@gmail.com>
2018-07-23 13:23:06 -0300

    PL_JB: [backend] Added junit test for section sorting

---
+++ app/src/test/java/com/example/peter/a3130project/SectionSortTest.java
@@ -0,0 +1,27 @@
+package com.example.peter.a3130project;
+
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Contributors: PL, JB
+ *
+ * Tests for register business logic
+ */
+public class SectionSortTest {
+    
+    /** Tests **/
+    @Test
+    public void sortClassesBySection() {
+	
+	assertTrue(false);
+	
+    }
+
+
+}
